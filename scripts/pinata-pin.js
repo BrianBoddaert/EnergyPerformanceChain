792ddf695f2a3214a341cd13cd43efb6bd207b39
AliyevRufat <rufataliyev1@outlook.fr>
2023-05-07 19:10:36 +0200

    Wrapped up pinning img/metadata to pinata IPFS
    Now we can pin images to pinata, create metadata (json) locally and pin them to pinata as well with the correct image CID included

--- scripts/pinata-pin.js
+++ scripts/pinata-pin.js
@@ -5,9 +5,13 @@ const basePathConverter = require("base-path-converter");
 const axios = require('axios');
 const JWT = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIzNGM1Mjk1My0yMzViLTQ0YzItYmMyNS1lYjQwMWNiM2E5MDMiLCJlbWFpbCI6InJ1ZmF0YWxpeWV2MUBvdXRsb29rLmZyIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siaWQiOiJGUkExIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9LHsiaWQiOiJOWUMxIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6IjQ1YTczODFlYTk2NmNkMWFkNjQwIiwic2NvcGVkS2V5U2VjcmV0IjoiMzUwZWI3MzVkYTc5NjczMzhmNzlmMTNlNGQ0YTlmZTVhMDE0YjZhMGU0MjRkYWY1ZDA5NzZjNTQ2MWFiMzliZiIsImlhdCI6MTY4MjE3OTEwOH0.u0-18e7HTi5s6qBlGZt7AENFzu0ro23X3e-xG1KfwXA'
 
-const pinImagesToPinata = async () => {
+//CID vars
+let imgFolderCID = "";
+
+const pinImagesToPinata = async () => 
+{
   const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
-  const src = "../img";
+  const src = "../Images";
   try {
     const { dirs, files } = await rfs.read(src);
     let data = new FormData();
@@ -21,19 +25,21 @@ const pinImagesToPinata = async () => {
           "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
           "Authorization": JWT
         },
-        onUploadProgress: progressEvent => {
-          console.log(progressEvent);
-        }
+        // onUploadProgress: progressEvent => {
+        //   console.log(progressEvent);
+        // }
       })
-    console.log(JSON.parse(response.body));
+    console.log("Pin image succeeded!"); // this logs the CID key
+    return response.data.IpfsHash;
   } catch (error) {
-    console.log(error);
+    console.log("Pin image failed!");
   }
 };
 
-const pinDataToPinata = async () => {
-  const url = `https://api.pinata.cloud/pinning/pinata-pin`;
-  const src = "../data";
+const pinMetaDataToPinata = async () => 
+{
+  const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+  const src = "../Data";
   try {
     const { dirs, files } = await rfs.read(src);
     let data = new FormData();
@@ -50,15 +56,75 @@ const pinDataToPinata = async () => {
         "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
         "Authorization": JWT
       },
-      onUploadProgress: progressEvent => {
-        console.log(progressEvent);
-      }
+      // onUploadProgress: progressEvent => {
+      //   console.log(progressEvent);
+      // }
     });
-    console.log(response.data);
+    console.log("Pin metadata succeeded!");
   } catch (error) {
-    console.log("error");
+    console.log("Pin metadata failed!");
   }
 };
 
-pinImagesToPinata()
-//pinDataToPinata();
+const createImages = async (_id) =>
+{
+
+}
+
+const createMetadata = async (_id) =>
+{
+  // Define the metadata entries
+  const metadata = {
+    name: "companyName",
+    description: "givenDescription",
+    image: "ipfs://" + imgFolderCID + "/" + _id + ".png",
+    attributes: [
+      {
+        energyEfficiency: "givenValue"
+      }
+    ]
+  };
+
+
+  // Convert the metadata object to a JSON string
+  const metadataJson = JSON.stringify(metadata);
+
+  // Define the file path and name
+  const filePath = '../Data/' + _id + '.json';
+
+  // Write the metadata JSON to a file
+  fs.writeFile(filePath, metadataJson, err => {
+    if (err) {
+      console.error("Metadata creation failed!");
+    } else {
+      console.log(`Metadata file created in ${filePath}!`);
+    }
+  });
+}
+
+const mainFunction = async () =>
+{
+  //creating images in the ../imgFolder for each company based on smart contract read values
+  // for (let i = 0; i < 100; i++)
+  // {
+  //   createImages();
+  // }
+
+  //pinning the imgFolder to pinata IPFS
+  imgFolderCID = await pinImagesToPinata();
+
+  //creating metadata in the ../data for each company based on smart contract read values and created images
+  //hardcoded to a for loop of 2 for now
+  for (let i = 1; i <= 2; i++) 
+  {
+    createMetadata(i);
+  }
+
+  // wait for 10 seconds before pinning the metadata to Pinata
+  setTimeout(() => { pinMetaDataToPinata(); }, 10000);
+}
+
+mainFunction();
+
+
+//TODO: change folder paths of both data and img to be the current year-month instead of hardcoded
