fe66c79e406ef9f40309c454f0db2df89dcc1435
BrianBoddaert <brian.boddaert@outlook.com>
2023-05-09 11:52:06 +0200

    Fixed online Json check by checking "image" attribute

--- apy.js
+++ apy.js
@@ -6,69 +6,107 @@ const https = require('https');
 
 async function GetAllJsonsInFolder()
 {
-    var Result = [];
+  var Result = [];
 
-    // const baseUrl = "https://gateway.pinata.cloud/ipfs/QmdfesFXsAZpS7hSvYUMM1dVQbqTYxK2g4iz35JbPcAyQ8"; //1.json
+  const baseUrl = "https://gateway.pinata.cloud/ipfs/QmdfesFXsAZpS7hSvYUMM1dVQbqTYxK2g4iz35JbPcAyQ8"; //1.json
 
-    // let i = 1;
-    // let url = baseUrl + `/${i}.json`;
+  let i = 1;
+  let url = baseUrl + `/${i}.json`;
 
-    // //Result[i-1] = await GetJsonFromURL(url);
+  for (let j = 0; j < 3; j++)
+  {
+    if (!(await isJsonWithImage(url)))
+    {
+      break;
+    }
 
-    // while (isUrlJson(url)) 
-    // {
-    //    Result[i-1] = await GetJsonFromURL(url);
-    //    i++;
-    //    url = baseUrl + `/${i}.json`;
-    // }
+    Result[i-1] = await GetJsonFromURL(url);
+    i++;
+    url = baseUrl + `/${i}.json`;
+  }
+  // while (isJsonWithImage(url)) 
+  // { 
+  //   Result[i-1] = GetJsonFromURL(url);
+  //   i++;
+  //   url = baseUrl + `/${i}.json`;
+  // }
 
-    return Result;
+  return Result;
 }
 
-async function isUrlJson(url) 
+async function isJsonWithImage(url) 
 {
-    try 
-    {
-        
-      const response = await https.get(url);
-      const contentType = response.headers['content-type'];
-      if (contentType.includes('application/json')) {
+  try {
+    const response = await new Promise((resolve, reject) => {
+      https.get(url, resolve).on('error', reject);
+    });
+
+    const contentType = response.headers['content-type'];
+    
+    if (contentType && contentType.includes('application/json')) {
+      const data = await new Promise((resolve, reject) => {
+        let json = '';
+
+        response.on('data', chunk => {
+          json += chunk;
+        });
+
+        response.on('end', () => {
+          try {
+            const jsonData = JSON.parse(json);
+            resolve(jsonData);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      });
+
+      if (data.hasOwnProperty('image')) {
         return true;
       } else {
         return false;
       }
-    } 
-    catch (error) 
-    {
-      console.log(`Error checking URL ${url}: ${error}`);
+    } else {
       return false;
     }
+  } catch (error) {
+    console.error(`Error checking if ${url} is JSON with image property: ${error.message}`);
+    return false;
+  }
 
 }
 
 async function GetJsonFromURL(url)
 {
-    // return new Promise((resolve, reject) => {
-    //     https.get(url, (response) => {
-    //       let data = '';
+
+    // const testString = '{ "name":"SampleProj #1", "description":"These are my sampleproj pictures", "image":"ipfs://QmXhbZLVCsjdKaCz5Cw7kot3P3unBawrbzUPLcULrimamQ/1.png","attributes": [ {"trait_type":"Color","value":"Black&White"},{"trait_type":"Background","value":"Clouds"}],"external_url":""}';
+    // const json = JSON.parse(testString);
+    // json.image = 'https://ipfs.io/ipfs/' + json.image.substring(7);
+
+    // return json;
+
+    return new Promise((resolve, reject) => {
+        https.get(url, (response) => {
+          let data = '';
     
-    //       response.on('data', (chunk) => {
-    //         data += chunk;
-    //       });
+          response.on('data', (chunk) => {
+            data += chunk;
+          });
     
-    //       // response.on('end', () => {
-    //       //   try {
-    //       //     // const json = JSON.parse(data);
-    //       //     // json.image = 'https://ipfs.io/ipfs/' + json.image.substring(7);
-    //       //     resolve();
-    //       //   } catch (error) {
-    //       //     reject(error);
-    //       //   }
-    //       // });
-    //     }).on('error', (error) => {
-    //       reject(error);
-    //     });
-    //   });
+          response.on('end', () => {
+            try {
+              const testString = '{ "name":"SampleProj #1", "description":"These are my sampleproj pictures", "image":"ipfs://QmXhbZLVCsjdKaCz5Cw7kot3P3unBawrbzUPLcULrimamQ/1.png","attributes": [ {"trait_type":"Color","value":"Black&White"},{"trait_type":"Background","value":"Clouds"}],"external_url":""}';
+              const json = JSON.parse(data);
+              json.image = 'https://ipfs.io/ipfs/' + json.image.substring(7);
+              resolve();
+            } catch (error) {
+              reject(error);
+            }
+          });
+        }).on('error', (error) => {
+          reject(error);
+        });
+      });
 
 }
 
