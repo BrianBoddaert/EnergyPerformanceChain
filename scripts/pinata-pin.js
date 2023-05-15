464d0c972ced3cf157c63379bede32a8ef409c6c
AliyevRufat <rufataliyev1@outlook.fr>
2023-05-15 22:39:53 +0200

    Added smart contract interaction in JS
    The whole process is now happening in one main function. The registering and/or updating of companies to smart contract, image creation and pinning, metadata creation and pinning, CID update and NFT minting

--- scripts/pinata-pin.js
+++ scripts/pinata-pin.js
@@ -1,12 +1,29 @@
+//Requires
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const fs = require("fs");
+const csv = require('csv-parser');
 const FormData = require("form-data");
 const rfs = require("recursive-fs");
 const basePathConverter = require("base-path-converter");
 const axios = require('axios');
-const JWT = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIzNGM1Mjk1My0yMzViLTQ0YzItYmMyNS1lYjQwMWNiM2E5MDMiLCJlbWFpbCI6InJ1ZmF0YWxpeWV2MUBvdXRsb29rLmZyIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siaWQiOiJGUkExIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9LHsiaWQiOiJOWUMxIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6IjQ1YTczODFlYTk2NmNkMWFkNjQwIiwic2NvcGVkS2V5U2VjcmV0IjoiMzUwZWI3MzVkYTc5NjczMzhmNzlmMTNlNGQ0YTlmZTVhMDE0YjZhMGU0MjRkYWY1ZDA5NzZjNTQ2MWFiMzliZiIsImlhdCI6MTY4MjE3OTEwOH0.u0-18e7HTi5s6qBlGZt7AENFzu0ro23X3e-xG1KfwXA'
-
+//Pinata details
+const JWT = process.env.JWT
+//Web3 details
+const Web3 = require('web3');
+const web3 = new Web3(process.env.INFURA_URL);
+//Contract details
+const contractABI = require('../SmartContracts/EPChain-abi.json'); //Should be updated if we deploy a new, updated smart contract
+const contractAddress = '0xdC55902925266A8ccC7972143789A06e5616FC65' //This has to be deployed smart contract address on the GOERLI testnet
+const EPChainContract = new web3.eth.Contract(contractABI, contractAddress);
+//Wallet/Account details
+const privateKey = process.env.PRIVATE_KEY; //This should be updated if you use a different account/wallet
+const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+web3.eth.accounts.wallet.add(account);
+web3.eth.defaultAccount = account.address;
 //CID vars
 let imgFolderCID = "";
+let dataFolderCID = "";
 let date = "202305" //set it every time //TODO: set the date on folder names of images and data as well
 
 const pinImagesToPinata = async () => 
@@ -62,6 +79,7 @@ const pinMetaDataToPinata = async () =>
       // }
     });
     console.log("Pin metadata succeeded!");
+    return response.data.IpfsHash;
   } catch (error) {
     console.log("Pin metadata failed!");
   }
@@ -103,26 +121,87 @@ const createMetadata = async (_id) =>
   });
 }
 
+const readCSVFileAndRegisterOrUpdateCompanies = async () => {
+  const stream = fs.createReadStream('../CompanyInfo.csv')
+    .pipe(csv());
+
+  for await (const row of stream) {
+    const { ID, Value, Address } = row;
+
+    await new Promise((resolve, reject) => {
+      EPChainContract.methods.updateOrRegisterCompany(ID, Value, Address).send({
+        from: '0x972B4B46e0baBb59fE2cA41ef3D6aBFA2741623d',
+        gas: 3000000,
+      })
+      .on('receipt', (receipt) => {
+        // Transaction completed successfully
+        resolve(receipt);
+      })
+      .on('error', (error) => {
+        // Transaction failed
+        reject(error);
+      });
+    });
+  }
+
+  console.log('CSV file processed');
+};
+
+const mintNFTs = async () =>
+{
+  await EPChainContract.methods.mintForRegisteredCompanies(date).send
+  (
+    {
+      from : '0x972B4B46e0baBb59fE2cA41ef3D6aBFA2741623d',
+      gas: 3000000,
+    }
+  )
+}
+
+const updateCIDValueOnSmartContract = async () =>
+{
+  console.log(dataFolderCID);
+  await EPChainContract.methods.setBaseURL(dataFolderCID).send
+  (
+    {
+      from : '0x972B4B46e0baBb59fE2cA41ef3D6aBFA2741623d',
+      gas: 3000000,
+    }
+  )
+}
+
 const mainFunction = async () =>
 {
-  //creating images in the ../imgFolder for each company based on smart contract read values
+  //Reading the CompanyInfo.csv file and registering/updating the companies to the smart contract
+  await readCSVFileAndRegisterOrUpdateCompanies();
+
+  //Creating images in the ../imgFolder for each company based on smart contract read values
   // for (let i = 0; i < 100; i++)
   // {
   //   createImages();
   // }
 
-  //pinning the imgFolder to pinata IPFS
+  //Pinning the imgFolder to pinata IPFS
   imgFolderCID = await pinImagesToPinata();
 
-  //creating metadata in the ../data for each company based on smart contract read values and created images
-  //hardcoded to a for loop of 1 for now
-  for (let i = 1; i <= 1; i++) 
+  //Creating metadata in the ../data for each company based on smart contract read values and created images
+  //Hardcoded to a for loop of 1 for now
+  for (let i = 1; i <= 3; i++) 
   {
     createMetadata(i);
   }
 
-  // wait for 10 seconds before pinning the metadata to Pinata
-  setTimeout(() => { pinMetaDataToPinata(); }, 10000);
+  //Wait for 10 seconds before pinning the metadata to Pinata
+  dataFolderCID = await setTimeout(() => { pinMetaDataToPinata(); }, 10000);
+
+  //Setting the new CID of the metadata on the smart contract
+  await updateCIDValueOnSmartContract();
+
+  //Minting the NFT's for all registered companies
+  setTimeout(() => { mintNFTs(); }, 10000);
 }
 
+//Calling the main function every month
+//const interval = setInterval(mainFunction(), 30 * 24 * 60 * 60 * 1000);
 mainFunction();
+//Use PROMISE functionality everywhere where needed just like in readCSVFileAndRegisterOrUpdateCompanies() function
