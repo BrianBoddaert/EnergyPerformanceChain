2a4baa3f119de62fa125eaf9d2d6c2c2dac42dd6
PaulYoSwek <Paulgoal2@gmail.com>
2023-05-16 21:40:03 +0200

    Created detail page

--- pages/header.js
+++ pages/header.js
@@ -9,7 +9,7 @@ export function Header() {
                 </div>
             </div>
             <div className={`${styles.buttonsholder}`}>
-                <a href="/dashboard"><button>Dashboard</button></a>
+                <a href="/"><button>Dashboard</button></a>
                 <a href="/about"><button>About us</button></a>
                 <a href="/faq"><button>FAQ</button></a>
             </div>
