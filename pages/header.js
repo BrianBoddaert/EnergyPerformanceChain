43b5e69aaaaf4fb3717b1f9082147462b9daebfa
PaulYoSwek <Paulgoal2@gmail.com>
2023-05-25 22:54:19 +0200

    Changed layout of the header / home page

--- pages/header.js
+++ pages/header.js
@@ -4,47 +4,48 @@ import { useEffect } from 'react';
 export function Header() {
   useEffect(() => {
     const body = document.body;
-    body.style.backgroundImage = 'var(--background-image-light)';
+    //body.style.backgroundImage = 'var(--background-image-light)';
 
-    const toggleMode = () => {
-      const isDarkMode = body.classList.toggle('dark-mode');
-      const textElement = document.querySelector('.text-element');
+  //   const toggleMode = () => {
+  //     const isDarkMode = body.classList.toggle('dark-mode');
+  //     const textElement = document.querySelector('.text-element');
 
-      // Change background image based on mode
-      const backgroundImage = isDarkMode ? 'var(--background-image-dark)' : 'var(--background-image-light)';
-      body.style.backgroundImage = backgroundImage;
+  //     // Change background image based on mode
+  //     //const backgroundImage = isDarkMode ? 'var(--background-image-dark)' : 'var(--background-image-light)';
+  //     //body.style.backgroundImage = backgroundImage;
 
-      // Change text color based on mode
-      const textColor = isDarkMode ? 'var(--text-color-light)' : 'var(--text-color-dark)';
-      body.style.color = textColor;
+  //     // Change text color based on mode
+  //     //const textColor = isDarkMode ? 'var(--text-color-light)' : 'var(--text-color-dark)';
+  //     //body.style.color = textColor;
 
-      textElement.src = isDarkMode ? 'Images/lightoff.png' : 'Images/lighton.png';
-    };
+  //     //textElement.src = isDarkMode ? 'Images/lightoff.png' : 'Images/lighton.png';
+  //   };
 
-    const textElement = document.querySelector('.text-element');
-    textElement.addEventListener('click', toggleMode);
+  //   // <img src='Images/lighton.png' className={`text-element ${styles.lightmode}`} />
 
-    return () => {
-      textElement.removeEventListener('click', toggleMode);
-    };
+  //   const textElement = document.querySelector('.text-element');
+  //   textElement.addEventListener('click', toggleMode);
+
+  //   return () => {
+  //     textElement.removeEventListener('click', toggleMode);
+  //   };
   }, []);
 
     return(
-        <div className={`row mt-4 ${styles.Main}`}>
-            <div>
-                <img src='Images/lighton.png' className={`text-element ${styles.lightmode}`} />
-                <div className={`row mt-4 ${styles.headerbackground}`}>
-                    <div className='col-sm-12'>
-                        <img src='Images/LogoTransp.png' />
-                    </div>
-                </div>
+        <div className={`${styles.Main}`}>
+          <img className={`${styles.headerBackgroundImage}`} src='Images/HeaderBackground.png'></img>
+          <div className={`${styles.headerContent}`}>
+            <div className={`${styles.headerImage}`}>
+              <img src='Images/LogoTransp.png' />
+            </div>
+              <h3 className={`${styles.headerTitle}`}>Fair Energy Certification</h3>
             </div>
             <div className={`${styles.buttonsholder}`}>
-                <a href="/"><button>Dashboard</button></a>
-                <a href="/about"><button>About us</button></a>
-                <a href="/faq"><button>FAQ</button></a>
-                <a href="/register"><button>Register</button></a>
-            </div>
+                  <a href="/"><button>Dashboard</button></a>
+                  <a href="/about"><button>About us</button></a>
+                  <a href="/faq"><button>FAQ</button></a>
+                  <a href="/register"><button>Register</button></a>
+              </div>
         </div>
     );
 }
